85d97795a65d4a415f3e61a0e68507e052828b94
Denis T. Nishida <denis.nishida@gmail.com>
2017-02-18 12:46:44 -0600

    Lesson 6 - Routes and Database

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function ()
 {
-    $names = ['Cena', 'Undertaker', 'Wyatt'];
+    //$names = ['Cena', 'Undertaker', 'Wyatt'];
+    //$names = DB::table('names')->latest()->get();
 
-    return view('welcome', compact('names'));
+    //return $names;
+    // return view('welcome', compact('names'));
+    return view('welcome');
+});
+
+Route::get('/names', function ()
+{
+    //$names = ['Cena', 'Undertaker', 'Wyatt'];
+    $names = DB::table('names')->latest()->get();
+
+    //return $names;
+    return view('names.index', compact('names'));
+});
+
+Route::get('/names/{name}', function($id)
+{
+    $name = DB::table('names')->find($id);
+    //dd($name);
+    return view('names.show', compact('name'));
 });
 
 Route::get('about', function ()
